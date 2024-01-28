4cbb4509fe85bcd7e4ff13c2dd37d06fa9d3a096
Roni POGGIOLI <roni.poggioli@brassart.fr>
2024-01-28 12:11:40 +0100

    build sans wise

---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/V1/UHT/MyWorldSettings.gen.cpp
@@ -0,0 +1,91 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "V1/Public/MyWorldSettings.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeMyWorldSettings() {}
+// Cross Module References
+	ENGINE_API UClass* Z_Construct_UClass_AWorldSettings();
+	UPackage* Z_Construct_UPackage__Script_V1();
+	V1_API UClass* Z_Construct_UClass_AMyWorldSettings();
+	V1_API UClass* Z_Construct_UClass_AMyWorldSettings_NoRegister();
+// End Cross Module References
+	void AMyWorldSettings::StaticRegisterNativesAMyWorldSettings()
+	{
+	}
+	IMPLEMENT_CLASS_NO_AUTO_REGISTRATION(AMyWorldSettings);
+	UClass* Z_Construct_UClass_AMyWorldSettings_NoRegister()
+	{
+		return AMyWorldSettings::StaticClass();
+	}
+	struct Z_Construct_UClass_AMyWorldSettings_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UECodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UECodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AMyWorldSettings_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AWorldSettings,
+		(UObject* (*)())Z_Construct_UPackage__Script_V1,
+	};
+#if WITH_METADATA
+	const UECodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AMyWorldSettings_Statics::Class_MetaDataParams[] = {
+		{ "Comment", "/**\n * \n */" },
+		{ "HideCategories", "Actor Advanced Display Events Object Attachment Info Input Blueprint Layers Tags Replication Input Movement Collision Transformation HLOD DataLayers" },
+		{ "IncludePath", "MyWorldSettings.h" },
+		{ "ModuleRelativePath", "Public/MyWorldSettings.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AMyWorldSettings_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AMyWorldSettings>::IsAbstract,
+	};
+	const UECodeGen_Private::FClassParams Z_Construct_UClass_AMyWorldSettings_Statics::ClassParams = {
+		&AMyWorldSettings::StaticClass,
+		"game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002A4u,
+		METADATA_PARAMS(Z_Construct_UClass_AMyWorldSettings_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_AMyWorldSettings_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AMyWorldSettings()
+	{
+		if (!Z_Registration_Info_UClass_AMyWorldSettings.OuterSingleton)
+		{
+			UECodeGen_Private::ConstructUClass(Z_Registration_Info_UClass_AMyWorldSettings.OuterSingleton, Z_Construct_UClass_AMyWorldSettings_Statics::ClassParams);
+		}
+		return Z_Registration_Info_UClass_AMyWorldSettings.OuterSingleton;
+	}
+	template<> V1_API UClass* StaticClass<AMyWorldSettings>()
+	{
+		return AMyWorldSettings::StaticClass();
+	}
+	AMyWorldSettings::AMyWorldSettings(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {}
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AMyWorldSettings);
+	AMyWorldSettings::~AMyWorldSettings() {}
+	struct Z_CompiledInDeferFile_FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_Statics
+	{
+		static const FClassRegisterCompiledInInfo ClassInfo[];
+	};
+	const FClassRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_Statics::ClassInfo[] = {
+		{ Z_Construct_UClass_AMyWorldSettings, AMyWorldSettings::StaticClass, TEXT("AMyWorldSettings"), &Z_Registration_Info_UClass_AMyWorldSettings, CONSTRUCT_RELOAD_VERSION_INFO(FClassReloadVersionInfo, sizeof(AMyWorldSettings), 3880493955U) },
+	};
+	static FRegisterCompiledInInfo Z_CompiledInDeferFile_FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_1369687892(TEXT("/Script/V1"),
+		Z_CompiledInDeferFile_FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_Statics::ClassInfo, UE_ARRAY_COUNT(Z_CompiledInDeferFile_FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_Statics::ClassInfo),
+		nullptr, 0,
+		nullptr, 0);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ Intermediate/Build/Win64/UnrealEditor/Inc/V1/UHT/MyWorldSettings.generated.h
@@ -0,0 +1,98 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "MyWorldSettings.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef V1_MyWorldSettings_generated_h
+#error "MyWorldSettings.generated.h already included, missing '#pragma once' in MyWorldSettings.h"
+#endif
+#define V1_MyWorldSettings_generated_h
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_SPARSE_DATA
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_RPC_WRAPPERS
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_ACCESSORS
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAMyWorldSettings(); \
+	friend struct Z_Construct_UClass_AMyWorldSettings_Statics; \
+public: \
+	DECLARE_CLASS(AMyWorldSettings, AWorldSettings, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/V1"), NO_API) \
+	DECLARE_SERIALIZER(AMyWorldSettings)
+
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAMyWorldSettings(); \
+	friend struct Z_Construct_UClass_AMyWorldSettings_Statics; \
+public: \
+	DECLARE_CLASS(AMyWorldSettings, AWorldSettings, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/V1"), NO_API) \
+	DECLARE_SERIALIZER(AMyWorldSettings)
+
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyWorldSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyWorldSettings) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyWorldSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyWorldSettings); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyWorldSettings(AMyWorldSettings&&); \
+	NO_API AMyWorldSettings(const AMyWorldSettings&); \
+public: \
+	NO_API virtual ~AMyWorldSettings();
+
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AMyWorldSettings(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AMyWorldSettings(AMyWorldSettings&&); \
+	NO_API AMyWorldSettings(const AMyWorldSettings&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AMyWorldSettings); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AMyWorldSettings); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AMyWorldSettings) \
+	NO_API virtual ~AMyWorldSettings();
+
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_12_PROLOG
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_SPARSE_DATA \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_RPC_WRAPPERS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_ACCESSORS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_INCLASS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_SPARSE_DATA \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_ACCESSORS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_INCLASS_NO_PURE_DECLS \
+	FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> V1_API UClass* StaticClass<class AMyWorldSettings>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_GGJ2024_V2_Source_V1_Public_MyWorldSettings_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
